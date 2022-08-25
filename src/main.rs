3261e5a90c3009f3699ed14aaba1857b98aeecba
eliJI <elijahingr@gmail.com>
2022-08-25 11:02:11 -0400

    added parsing
    parsing added

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,41 @@ use std::fs::File;
 use std::thread;
 use std::time::Duration;
 use std::path::Path;
+use std::result;
 fn main() {
-    let quotes:Vec<String> = Vec::new();
-    //let path = Path::new("\\logo.txt");
+    displayTitle();
+    
+    thread::sleep(Duration::from_millis(5000));
+    println!("done");
+}
+
+fn displayTitle() {
     let f = File::open("logo.txt").expect("File failed to open");
     let mut reader = BufReader::new(f);
     let line = String::new();
     let lines = reader.lines();
-
-
     
     for l in lines {
        match l {
-        Ok(s) => {println!("{}",s)},
+        Ok(s) => println!("{}",s),
         Err(e) => println!("ERROR")
        }
     }
-    thread::sleep(Duration::from_millis(5000));
-    println!("done");
+    
 }
+
+fn displayQuote() {
+    let f = File::open("bubsy.txt").expect("File failed to open");
+    let mut reader = BufReader::new(f);
+    let lines = reader.lines();
+    let mut collected_lines:Vec<String> = Vec::new();
+    for l in lines {
+        match l {
+            Ok(s) => collected_lines.push(s),
+            Err(e) => println!("Error Occured: {e}")
+        }
+    }
+
+}
+  
 
